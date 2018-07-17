8d7855491067817ec9b0b57e8a07f5b4d8a4c48d
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-07-17 15:14:12 +0300

    fixup! Simplify parsing grammar

--- ktor-http/src/io/ktor/http/parsing/Primitives.kt
+++ ktor-http/src/io/ktor/http/parsing/Primitives.kt
@@ -1,10 +1,10 @@
 package io.ktor.http.parsing
 
-internal val lowAlpha = 'a' to 'z'
-internal val alpha = ('a' to 'z') or ('A' to 'Z')
-internal val digit = RawGrammar("\\d")
-internal val hex = digit or ('A' to 'F') or ('a' to 'f')
+internal val lowAlpha get() = 'a' to 'z'
+internal val alpha get() = ('a' to 'z') or ('A' to 'Z')
+internal val digit get() = RawGrammar("\\d")
+internal val hex get() = digit or ('A' to 'F') or ('a' to 'f')
 
-internal val alphaDigit = alpha or digit
-internal val alphas = atLeastOne(alpha)
-internal val digits = atLeastOne(digit)
+internal val alphaDigit get() = alpha or digit
+internal val alphas get() = atLeastOne(alpha)
+internal val digits get() = atLeastOne(digit)
